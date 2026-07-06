a9102c586fa836b00fa120e8ae2ee748a4a6dbe4
Aileen7542 <a01277542@tec.mx>
2026-07-06 01:52:36 +0000

    HPP + CPP

---
+++ Personaje.cpp
@@ -0,0 +1,5 @@
+#include Personaje
+#include <iostream>
+
+Personaje::Personaje();
+Personaje::Personaje(double); p()
---
+++ Personaje.hpp
@@ -0,0 +1,28 @@
+#pragma once 
+
+class Personaje
+{
+    private: 
+        float vidaMax; 
+        float salud; 
+        float ataque; 
+        int nivel; 
+
+    public: 
+        Personaje();
+        Personaje(double); 
+        double getVidaMax() const;
+        void setVidaMax(double);
+        double getSalud() const; 
+        void setSalud(double); 
+        double getAtaque(); 
+        void setAtaque(double); 
+        double getNivel(); 
+        void setNivel(double);
+        int porcentajeSalud(); 
+        void imprimeBarra();
+        int calculaAtaque(Personaje& objetivo); 
+        void recibeAtaque(int ptosAtaque);
+        void atacar(Personaje& objetivo);
+        void imprime();
+};
