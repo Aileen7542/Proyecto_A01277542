fcff23082e4aab5392f9338fcd659b6a4c98b56a
Aileen7542 <a01277542@tec.mx>
2026-07-13 02:37:28 +0000

    HPP

--- .devcontainer/Hechicero.hpp
+++ .devcontainer/Hechicero.hpp
@@ -4,29 +4,14 @@
 
 class Hechicero: public Personaje {
     private: 
-        float vidaMax; 
-        float salud; 
-        float ataque; 
-        int nivel;
         float magia; 
 
     public: 
         Hechicero(); 
         Hechicero(float, float, float, int, float); 
-        float getVidaMax() const;
-        void setVidaMax(float);
-        float getSalud() const; 
-        void setSalud(float); 
-        float getAtaque() const; 
-        void setAtaque(float); 
-        int getNivel() const; 
-        void setNivel(int);
         float getMagia(); 
         void setMagia(float); 
-        int porcentajeSalud(); 
-        void imprimeBarra();
         int calculaAtaque(Hechicero& otroHechicero); 
         void recibeAtaque(int ptosAtaque);
-        void atacar(Hechicero& otroHechicero);
         void imprime() const;
 }; 
