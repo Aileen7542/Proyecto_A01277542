457686de1a2de7b4107c0f0a3c5e463d64ad6d12
Aileen7542 <a01277542@tec.mx>
2026-07-20 03:09:25 +0000

    cpp

--- assignments/Proyecto/exercise.cpp
+++ assignments/Proyecto/exercise.cpp
@@ -1,41 +1,23 @@
 #include <iostream>
+#include <vector>
+#include "Personaje.hpp"
 #include "Caballero.hpp"
 #include "Guerrero.hpp"
 #include "Hechicero.hpp"
 
-int main()
-{
-    Caballero Caballero(100, 100, 20, 5, 15, 10);
-    Guerrero Guerrero(120, 120, 25, 4, 30);
-    Hechicero Hechicero(80, 80, 35, 6, 40);
-
-    Caballero.imprime();
-    Guerrero.imprime();
-    Hechicero.imprime();
-
-    std::cout << "Caballero ataca a Guerrero" << std::endl;
-    Caballero.atacar(Guerrero);
-    Guerrero.imprime();
-
-    std::cout << "Guerrero ataca a Caballero" << std::endl;
-    Guerrero.atacar(Caballero);
-    Caballero.imprime();
-
-    std::cout << "Hechicero ataca a Caballero" << std::endl;
-    Hechicero.atacar(Caballero);
-    Caballero.imprime();
-
-    std::cout << "Caballero ataca a Hechicero" << std::endl;
-    Caballero.atacar(Hechicero);
-    Hechicero.imprime();
-
-    std::cout << "Guerrero ataca a Hechicero" << std::endl;
-    Guerrero.atacar(Hechicero);
-    Hechicero.imprime();
-
-    std::cout << "Hechicero ataca a Guerrero" << std::endl;
-    Hechicero.atacar(Guerrero);
-    Guerrero.imprime();
-
+int main() {
+    std::vector<Personaje*> personaje;
+    personaje.push_back(new Guerrero());
+    personaje.push_back(new Caballero());
+    personaje.push_back(new Hechicero());
+// personajees 
+    for (Personaje* p : personaje) {
+        std::cout << *p << std::endl;
+    }
+// peleaa
+    for (int i = 1; i < personaje.size(); i++) {
+        personaje[0]->atacar(*personaje[i]);
+        std::cout << *personaje[i] << std::endl;
+    }
     return 0;
 }
